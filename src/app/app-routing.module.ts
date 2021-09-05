13947c211cd62ad20fd24af898123b1a88e2b949
Danilo Fernandes <fernandes.danilo1996@gmail.com>
2021-09-05 10:55:14 -0400

    task 1 - html body implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddShipComponent } from './components/add-ship/add-ship.component';
 
-const routes: Routes = [];
+//components
+import { ListShipsComponent } from './components/list-ships/list-ships.component';
+
+const routes: Routes = [
+  { path: '', component: ListShipsComponent },
+  { path: 'add-ship', component: AddShipComponent },
+  { path: 'edit-ship/:id', component: AddShipComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
